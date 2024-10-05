62bcd45d2c77401fbb578ea25f5129fa737ced57
Ramia-aloufi <ramia.aloufi@gmail.com>
2024-10-05 13:40:36 +0300

    notFound Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { RecipeInformationComponent } from './pages/recipe-information/recipe-information.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {path:"recipe/:id",component:RecipeInformationComponent},
     {path:"",component:HomeComponent},
-    {path:"auth",component:AuthComponent}
-
+    {path:"auth",component:AuthComponent},
+    {path:"profile",component:UserProfileComponent},
+    { path: '**', component: NotFoundComponent }
 ];
